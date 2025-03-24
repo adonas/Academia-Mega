52c84f84741aabed022963b4129fb3496fec79f8
Adonai Rios Rodriguez <arios+megaev2@megacable.com.mx>
2025-03-24 15:38:41 -0600

    @->20250324T1537 Código de ejemplo de clase 01 de JScript, crear el código para desplegar en el console el nombre y la edad con base en definición de best pratices.
    Adonai Rios Rodríguez
    MEGACABLE Comunicaciones
    Gdl Jalisco MX
    
    Adicional se crea index.html para poder validar funcionalidad en navegador de alert.

---
+++ Modulo_1_Javascript_Testing/01_sesion/Adonai Rios/script.js
@@ -0,0 +1,22 @@
+alert("Hola a todos.")
+
+var int = 12345;
+var string = "texto";
+var boolean = false;
+
+var nombre = "Adonai";
+var apellido = "Rios";
+nombreCompleto = `${nombre} ${apellido}`;
+edad = 46;
+
+console.log(`Hola, soy ${nombreCompleto} y tengo ${edad} años `);
+//console.log("Hola soy " + nombreCompleto + " y tengo " + edad + " años" );
+
+//Comparación
+//== , ===, != , !== , <, > , <=, >= ;
+//var numero = 7;
+//var numeroTexto = "8";
+//console.log(numero + numeroTexto);
+
+//Actividad
+
