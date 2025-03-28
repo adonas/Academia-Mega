29d13707dbc5201105cabcb6bfdd1cbfeb487144
Adonai Rios Rodriguez <arios+megaev2@megacable.com.mx>
2025-03-28 15:20:59 -0600

    @->20250328T1520
    Adonai Rios Rodríguez
    Ejercicio usando HTML y CSS para clase 5 de JS

---
+++ Modulo_1_Javascript_Testing/05_sesion/Adonai Rios/Clase5.js
@@ -0,0 +1,10 @@
+
+//console.log("Código de js para clase 5")
+const titulo = document.getElementById("titulo")
+const parrafos = document.getElementById("parrafo01")
+
+console.log(titulo.textContent ) ;
+
+titulo.textContent = "Bienvenidos a mi página web, usando el DOM"
+
+parrafos.style.color = "blue"
