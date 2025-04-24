56db333bc1fad17c938915e2f8c6b7eb94241e13
Adonai Rios Rodriguez <arios+megaev2@megacable.com.mx>
2025-04-24 10:24:52 -0600

    @->20250424T1024 Ejercicios pendientes clase 3 y 4

--- Modulo_2_Angular/03_sesion/Adonai Rios/PROYECTO1/src/app/app.component.ts
+++ Modulo_2_Angular/03_sesion/Adonai Rios/PROYECTO1/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { saludo } from './Components/saludo/saludo.component';
 import { CardComponent } from './Components/card/card.component';
+import { TarjetaComponent } from "./Components/tarjeta/tarjeta.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , saludo , CardComponent],
+  imports: [RouterOutlet, saludo, CardComponent, TarjetaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,4 +17,33 @@ export class AppComponent {
   empresa = 'MEGACACABLE';
   nombre = 'Adonai Rios Rodríguez';
 
+  datillo: string | number = 1;
+  
+  carro: carModel = {
+    brand:'Ford',
+    model:'Focus' ,
+    year:2024
+  }
+
+  listCars: Array<any> = [
+    {
+      brand:'Chevrolter' , 
+      model:'Camaro',
+      year:2024
+    },
+    {
+      brand:'Ford' , 
+      model:'Mustang',
+      year:2024
+    }    
+
+  ];
+  
+
+}
+
+interface carModel {
+  brand:string;
+  model:string;
+  year?:number;
 }
