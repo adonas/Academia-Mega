70e78be711a760922bb8ae324d42556c24fec610
Adonai Rios Rodriguez <arios+megaev2@megacable.com.mx>
2025-04-28 15:38:09 -0600

    @->20250428T1537 Actiidades clase 5 y 6

---
+++ Modulo_2_Angular/05_sesion/Adonai Rios/PROYECTO1/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, model } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { saludo } from './Components/saludo/saludo.component';
+import { CardComponent } from './Components/card/card.component';
+import { TarjetaComponent } from "./Components/tarjeta/tarjeta.component";
+import { CommonModule } from '@angular/common';
+import { TodoComponent } from './Components/todo/todo.component';
+import { ProductManagerComponent } from './Components/product-manager/product-manager.component';
+@Component({
+  selector: 'app-root',
+  imports: [
+      RouterOutlet, 
+      saludo, 
+      CardComponent, 
+      TarjetaComponent, 
+      CommonModule, TodoComponent , ProductManagerComponent      
+    ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  //isVisible = true;
+  //frutas = ['Mazana','Platano','Naranja','Uva','Piña' , 'Arrayan'];
+
+  rolUsuario = "admin";
+
+  tareaImportante = true;
+  isUrgente = false;
+
+}
+
