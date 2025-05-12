6536ab0abe0ede707581c49a920b316d2004d313
Adonai Rios Rodriguez <arios+megaev2@megacable.com.mx>
2025-05-12 10:03:31 -0600

    @->20250512T1003 Entregable Clase 01 C# Liderly Adonai Rios Rodríguez

---
+++ Modulo_3_Dot_Net/01_sesion/Adonai Rios/HelloWorld/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+
+//@->20250512T0910 Mi primer programilla con Liderly con C#
+
+class Program
+{
+
+    private static Dictionary<string,string> usuarios = new Dictionary<string, string>
+    {
+        {"admin","qwerty"},
+        {"adonas","12345"},
+        {"test","test"}
+    };
+
+   static void Main ( string[] args ) 
+   {
+        //Mensaje de bienvenida
+        
+        Console.WriteLine("Este es el programa oficinal de Hola Mundo");
+        Console.WriteLine("Tienes que iniciar sesión \n");                
+        //Definir el usuario y la contraseña        
+
+         
+        Console.WriteLine("Escribe tu usuario");        
+        string usuarioCapturado = Console.ReadLine();
+
+
+        Console.WriteLine("Escribe tu contraseña");
+        String passCapturado = Console.ReadLine();
+
+            
+        if ( usuarios.ContainsKey(usuarioCapturado)  && usuarios[usuarioCapturado] ==  passCapturado)
+        {
+            Console.WriteLine("Autenticación conseguida de manera adecuada");
+            
+            for ( int i = 0 ; i<5; i++ )
+            {
+                Console.WriteLine($"{i} Hola usuario, gracias !!!");
+            }
+            
+        }
+        else
+        {
+            Console.WriteLine("Usuario o contarseña incorrecta, intente nuevamente.");
+        }
+    
+        Console.WriteLine("\n Presiona enter para salir del programa ");
+        Console.ReadLine();                
+
+
+   }
+}
+
+
+
+
