2205bb997c769cdec68ede4efe27b5d8ed5279c0
Adonai Rios Rodriguez <arios+megaev2@megacable.com.mx>
2025-06-08 21:11:44 -0600

    clase 20 pendiente que jale chido

---
+++ Modulo_3_Dot_Net/20_sesion/Adonai Rios/TaskManagerObserver/Program.cs
@@ -0,0 +1,15 @@
+﻿﻿using Microsoft.AspNetCore.SignalR.Client;
+using TaskManager.Shared.Events;
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("http://localhost:5162/taskEvents")
+    .Build();
+
+connection.On<TaskEvent>("TaskEvent", ev =>
+{
+    Console.WriteLine($"{ev.EventName}: {ev.Payload.Title}");
+});
+
+await connection.StartAsync();
+Console.WriteLine("Observando eventos...");
+await Task.Delay(Timeout.Infinite);
