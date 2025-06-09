6cf173124d9d803a115b359a290b05025befb32d
Adonai Rios Rodriguez <arios+megaev2@megacable.com.mx>
2025-06-09 08:32:23 -0600

    Ruta de tasks

--- Modulo_3_Dot_Net/20_sesion/Adonai Rios/TaskManagerClient/Helpers/ApiEndPoints.cs
+++ Modulo_3_Dot_Net/20_sesion/Adonai Rios/TaskManagerClient/Helpers/ApiEndPoints.cs
@@ -3,6 +3,6 @@ namespace TaskManagerClient.Helpers;
 public static class ApiEndpoints
 {
     public const string Base = "http://localhost:5162/api";
-    public const string Tasks = $"{Base}/Task";
+    public const string Tasks = $"{Base}/Tasks";
     
 }
