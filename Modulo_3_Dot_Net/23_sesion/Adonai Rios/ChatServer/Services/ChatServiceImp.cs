5444074519b33ab9eebed7407ed9f376a2b6c1be
Adonai Rios Rodriguez <arios+megaev2@megacable.com.mx>
2025-06-12 08:27:25 -0600

    Chat Server y ChatCLiente clase 23

---
+++ Modulo_3_Dot_Net/23_sesion/Adonai Rios/ChatServer/Services/ChatServiceImp.cs
@@ -0,0 +1,17 @@
+using Grpc.Core;
+using GrpcChat;
+
+namespace ChatServer.Services;
+
+public class ChatServiceImp : ChatService.ChatServiceBase
+{
+    private readonly ChatHub _hub;
+    private readonly ILogger<ChatServiceImp> _log;
+
+    public ChatServiceImp(ChatHub hub, ILogger<ChatServiceImp> log)
+        => (_hub, _log) = (hub, log);
+    
+            
+
+
+}
